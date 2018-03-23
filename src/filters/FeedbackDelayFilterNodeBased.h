ecf31129e700113dec8ac6ae22a18c808438bc2a
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2018-03-23 16:25:59 +0100

    Moved libs and added addon_config so the libs get correctly linked in xcode, also commented unused files that where automatically added to the project. With this commit you can update the xcode project that contains ofxPlaymodes and everything will be in place and working

--- src/filters/FeedbackDelayFilterNodeBased.h
+++ src/filters/FeedbackDelayFilterNodeBased.h
@@ -1,59 +1,60 @@
-/*
- * VideoMixerGPU.h
- *
- *  Created on: Jan 18, 2013
- *      Author: arturo
- */
+///*
+// * VideoMixerGPU.h
+// *
+// *  Created on: Jan 18, 2013
+// *      Author: arturo
+// */
+//
+//#ifndef FEEEDBACKDELAY_FILTER_H
+//#define FEEEDBACKDELAY_FILTER_H
+//
+//#include "VideoFilter.h"
+//#include "VideoBuffer.h"
+//#include "VideoHeader.h"
+//
+//namespace ofxPm{
+//    
+//class FeedbackDelayFilterNodeBased: public VideoFilter {
+//public:
+//    FeedbackDelayFilterNodeBased();
+//    virtual ~FeedbackDelayFilterNodeBased();
+//
+//    void setup(VideoSource & source1);
+//    void setupNodeBased();
+//    
+//    VideoFrame getNextVideoFrame();
+//    void newVideoFrame(VideoFrame & frame);
+//    float getFps(){return source->getFps();};
+//    void    setVideoBuffer(ofxPm::VideoBuffer* &videoBuffer);
+//
+//
+//private:
+//    VideoFrame      frame;
+//    ofFbo           fbo;
+//    VideoSource     *source;
+//    ofShader        shader;
+//    bool            newFrame;
+//    ofPlanePrimitive plane;
+//    //ofMutex swapBuffersMutex;
+//
+//    float fps;
+//    
+//    //    float gradientWidth;
+//    //    int gradientXorY;
+//    // FEATURE NODE
+//    ofParameterGroup*                   parameters;
+//    ofParameter<float>                  paramScale;
+//    ofParameter<float>                  paramMixAmmount;
+//    ofParameter<float>                  paramDryWet;
+//    ofParameter<ofxPm::VideoBuffer*>    paramVideoBufferInput;
+//    ofParameter<float>                  paramDelayMs;
+//    
+//    bool                                isNodeBased;
+//
+//    VideoFrame                          lastFrameRendered;
+//    VideoHeader                         videoHeaderDelay;
+//};
+//}
+//
+//#endif /* FEEEDBACKDELAY_FILTER_H */
 
-#ifndef FEEEDBACKDELAY_FILTER_H
-#define FEEEDBACKDELAY_FILTER_H
-
-#include "VideoFilter.h"
-#include "VideoBuffer.h"
-#include "VideoHeader.h"
-
-namespace ofxPm{
-    
-class FeedbackDelayFilterNodeBased: public VideoFilter {
-public:
-	FeedbackDelayFilterNodeBased();
-	virtual ~FeedbackDelayFilterNodeBased();
-
-    void setup(VideoSource & source1);
-    void setupNodeBased();
-    
-    VideoFrame getNextVideoFrame();
-	void newVideoFrame(VideoFrame & frame);
-    float getFps(){return source->getFps();};
-    void    setVideoBuffer(ofxPm::VideoBuffer* &videoBuffer);
-
-
-private:
-	VideoFrame      frame;
-    ofFbo           fbo;
-	VideoSource     *source;
-	ofShader        shader;
-	bool            newFrame;
-    ofPlanePrimitive plane;
-	//ofMutex swapBuffersMutex;
-
-    float fps;
-    
-    //    float gradientWidth;
-    //    int gradientXorY;
-    // FEATURE NODE
-    ofParameterGroup*                   parameters;
-    ofParameter<float>                  paramScale;
-    ofParameter<float>                  paramMixAmmount;
-    ofParameter<float>                  paramDryWet;
-    ofParameter<ofxPm::VideoBuffer*>    paramVideoBufferInput;
-    ofParameter<float>                  paramDelayMs;
-    
-    bool                                isNodeBased;
-
-    VideoFrame                          lastFrameRendered;
-    VideoHeader                         videoHeaderDelay;
-};
-}
-
-#endif /* FEEEDBACKDELAY_FILTER_H */
