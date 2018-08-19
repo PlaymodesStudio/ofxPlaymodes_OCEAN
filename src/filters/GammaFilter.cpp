1c01c412c9d64c97fdf8cff09636c70f4e6607c1
Eloi Maduell <eloi@playmodes.com>
2018-08-19 18:19:55 +0200

    added Gamma filter, something similar to a "Levels" effect.

---
+++ src/filters/GammaFilter.cpp
@@ -0,0 +1,110 @@
+#include "GammaFilter.h"
+
+namespace ofxPm{
+    
+    //--------------------------------------------------------------
+    GammaFilter::GammaFilter():ofxOceanodeNodeModel("Gamma FX")
+    {
+        setupNodeBased();
+    }
+
+    //--------------------------------------------------------------
+    GammaFilter::~GammaFilter()
+    {
+        fbo.clear();
+        shader.unload();
+    }
+
+    //--------------------------------------------------------------
+    void GammaFilter::setupNodeBased()
+    {
+        color = ofColor::darkMagenta;
+
+        source = NULL;
+        fps = -1;
+        
+        string shaderName = "shaders/gamma";
+        shader.load(shaderName);
+        cout << "GammaFilter::Loading Shader : " << shaderName << endl;
+        // allocate fbo where to draw
+        if (fbo.isAllocated())
+        {
+            fbo.allocate(source->getWidth(),source->getHeight(),GL_RGBA);
+        }
+                
+        parameters->add(paramFrameIn.set("Frame Input", frame));
+        parameters->add(paramMin.set("Min",0,0.0,1.0));
+        parameters->add(paramMax.set("Max",1.0,0.0,1.0));
+        parameters->add(paramGamma.set("Gamma",1.0,0.0,4.0));
+        parameters->add(paramFrameOut.set("Frame Output", frame));
+        
+        paramMin.addListener(this, &GammaFilter::setMin);
+        paramMax.addListener(this, &GammaFilter::setMax);
+        
+        paramFrameIn.addListener(this, &GammaFilter::newVideoFrame);
+        
+    }
+    //------------------------------------------------------------
+    void GammaFilter::update(ofEventArgs &e)
+    {
+        if(fboHasToBeAllocated != glm::vec2(-1, -1))
+        {
+            fbo.allocate(fboHasToBeAllocated.x, fboHasToBeAllocated.y);
+            fboHasToBeAllocated = glm::vec2(-1, -1);
+        }
+    }
+    
+
+    //--------------------------------------------------------------
+    //VideoFrame LumaFilterNodeBased::getNextVideoFrame()
+    //{
+    /*
+        if(source->getNextVideoFrame()!=NULL)
+        {
+            return source->getNextVideoFrame();
+        }
+        return frame;
+     */
+    //}
+
+    //--------------------------------------------------------------
+    void GammaFilter::newVideoFrame(VideoFrame & _frame)
+    {
+        bool isAllocated = fbo.isAllocated();
+        bool frameIsNull = _frame.isNull();
+        
+        if(!frameIsNull)
+        {
+            if (!isAllocated || _frame.getWidth() != fbo.getWidth() || _frame.getHeight() != fbo.getHeight())
+            {
+                fboHasToBeAllocated = glm::vec2(_frame.getWidth(), _frame.getHeight());
+            }
+
+            if(fbo.isAllocated())
+            {
+                fbo.begin();
+                {
+                    ofClear(0, 0, 0, 255);
+                    shader.begin();
+                    {
+                        shader.setUniformTexture("tex0",_frame.getTextureRef(),11);
+                        shader.setUniform1f("u_min",paramMin);
+                        shader.setUniform1f("u_max",paramMax);
+                        shader.setUniform1f("u_gamma",paramGamma);
+
+                        ofSetColor(255);
+                        int w = _frame.getWidth();
+                        int h = _frame.getHeight();
+                        
+                        _frame.getTextureRef().draw(0,0,w,h);
+                    }
+                    shader.end();
+                }
+                fbo.end();
+                
+                frame = VideoFrame::newVideoFrame(fbo);
+            }
+        }
+        paramFrameOut = frame;
+    }
+}
---
+++ src/filters/GammaFilter.h
@@ -0,0 +1,55 @@
+/*
+ * VideoMixerGPU.h
+ *
+ *  Created on: Jan 18, 2013
+ *      Author: arturo
+ */
+
+#ifndef VIDEOGAMMASHADERFX_H_
+#define VIDEOGAMMASHADERFX_H_
+
+#include "VideoFilter.h"
+#include "ofxOceanodeNodeModel.h"
+
+namespace ofxPm{
+    
+class GammaFilter: public VideoFilter,public ofxOceanodeNodeModel {
+public:
+	GammaFilter();
+	virtual ~GammaFilter();
+
+    void setupNodeBased();
+    void update(ofEventArgs &e) override;
+
+    
+    VideoFrame getNextVideoFrame(){return VideoFrame();};
+	void newVideoFrame(VideoFrame & frame);
+    float getFps(){return source->getFps();};
+    void setMin(float &f){paramMin=f;};
+    void setMax(float &f){paramMax=f;};
+    
+
+private:
+	VideoFrame      frame;
+    ofFbo           fbo;
+	VideoSource     *source;
+	ofShader        shader;
+	bool            newFrame;
+    ofPlanePrimitive plane;
+	//ofMutex swapBuffersMutex;
+    glm::vec2       fboHasToBeAllocated;
+
+    
+    float fps;
+    
+    // FEATURE NODE
+    ofParameter<float>                  paramMin;
+    ofParameter<float>                  paramMax;
+    ofParameter<float>                  paramGamma;
+//    ofParameter<ofxPm::VideoFrame>      paramFrameIn;
+//    ofParameter<ofxPm::VideoFrame>      paramFrameOut;
+
+};
+}
+
+#endif /* VIDEOGAMMASHADERFX_H_ */
