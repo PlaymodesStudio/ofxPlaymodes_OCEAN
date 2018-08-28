af11de08052b3393134bc717d7da3c28078d3537
Eloi Maduell <eloi@playmodes.com>
2018-08-28 17:01:07 +0200

    added VideoSwitcher and minor changes to  VideoSplitter

---
+++ src/filters/VideoSwitcher.h
@@ -0,0 +1,39 @@
+
+/*
+ * VideoMixerGPU.h
+ *
+ *  Created on: Jan 18, 2013
+ *      Author: arturo
+ */
+
+#ifndef VIDEOSWITCHER_H_
+#define VIDEOSWITCHER_H_
+
+#include "VideoFilter.h"
+#include "ofxOceanodeNodeModel.h"
+
+namespace ofxPm{
+    
+    class VideoSwitcher: public VideoFilter,public ofxOceanodeNodeModel {
+    public:
+        VideoSwitcher();
+        virtual ~VideoSwitcher();
+        
+        void setupNodeBased();
+        void update(ofEventArgs &e) override;
+        
+        
+        VideoFrame getNextVideoFrame(){};
+        void newVideoFrame(VideoFrame & frame);
+        void newVideoFrame2(VideoFrame & frame2);
+        float getFps(){return fps;};
+        
+    private:
+        
+        float fps;
+        ofParameter<ofxPm::VideoFrame>      paramFrameIn2;
+        ofParameter<int>                    paramSwitchTo;
+    };
+}
+
+#endif /* VIDEOSWITCHER_H_ */
