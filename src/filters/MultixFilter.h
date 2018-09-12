62aa335ee3eef04a9203cfd7792d37f03987a988
Eloi Maduell <eloi@playmodes.com>
2018-09-12 19:08:00 +0200

    Cleaned Multix

--- src/filters/MultixFilter.h
+++ src/filters/MultixFilter.h
@@ -20,45 +20,32 @@ class MultixFilter: public VideoSource,public ofxOceanodeNodeModel
 {
 public:
     MultixFilter();
-//    MultixFilter(ofxPm::VideoBuffer & buffer, int numHeaders=2);
-
     virtual ~MultixFilter();
 
-//    void    setup(ofxPm::VideoBuffer & buffer, int numHeaders=2);
     void    setupNodeBased();
-    void update(ofEventArgs &e) override;
+    void    update(ofEventArgs &e) override;
 
+    void                            updateValuesMs(vector<float> _vf);
+    void                            drawIntoFbo(int x, int y,int w, int h);
 
-    void    updateValuesPct(vector<float> _vf);
-    void    updateValuesMs(vector<float> _vf);
-    void    drawIntoFbo(int x, int y,int w, int h);
-
-    void    setNumHeaders(int numHeaders);
-    int     getNumHeaders();
-
+    ofxPm::VideoBufferNodeBased*    getVideoBuffer() const;
+    void                            changedVideoBuffer(ofxPm::VideoBufferNodeBased* &videoBuffer);
+    void                            setVideoHeader(vector<ofxPm::VideoHeaderNodeBased*> videoHeader);
     
-    ofxPm::VideoBufferNodeBased *getVideoBuffer() const;
-    void    changedVideoBuffer(ofxPm::VideoBufferNodeBased* &videoBuffer);
-    void    setVideoHeader(vector<ofxPm::VideoHeaderNodeBased*> videoHeader);
-    
-    ofMutex     mutex;
-    bool        isMinmaxBlend() const;
-    
-    // + 
-    void                                    newVideoFrame(VideoFrame & _frame);
-//    ofEvent<VideoFrame>                     newFrameEvent;
-    VideoFrame                              getNextVideoFrame();
-    float                                   getFps(){return paramVideoBufferInput.get()->getFps();};
+    ofMutex                         mutex;
 
-    void                                    setMinmaxBlend(bool &_b){paramMinMaxBlend = _b;}
-    void                                    setOpacityMode(int &_m){paramOpacityMode=_m;};
-    void                                    setOffsetBeatDiv(int _n){paramOffsetBeatDiv = _n;};
-    void                                    setOffsetBeatMult(int _n){paramOffsetBeatMult = _n;};
+    // +
+    void                            newVideoFrame(VideoFrame & _frame);
+    VideoFrame                      getNextVideoFrame();
+    float                           getFps(){return paramVideoBufferInput.get()->getFps();};
+    void                            setMinmaxBlend(bool &_b){paramMinMaxBlend = _b;}
+    void                            setOffsetBeatDiv(int _n){paramOffsetBeatDiv = _n;};
+    void                            setOffsetBeatMult(int _n){paramOffsetBeatMult = _n;};
 
-    void                                    recalculate(int &_i);
-    void                                    changedDistributionVector(vector<float> &_b);
-    void                                    changedManualOffsetMs(float &f);
-    
+    void                            recalculate(int &_i);
+    void                            changedManualOffsetMs(float &f);
+    //    void                                    changedDistributionVector(vector<float> &_b);
+
     // when global BPM changes, it will call this function so I can know the global bpm
     void    setBpm(float _bpm) override{myBPM=_bpm;int i=0;recalculate(i);};
 
@@ -67,31 +54,28 @@ protected:
     ofxPm::VideoHeaderNodeBased         videoHeader;
     vector<float>                       multixDelaysInMs;
     float                               myBPM;
-    //NodeBased
 
     ofParameter<int>                    paramNumHeaders;
-    ofParameter<int>                    paramOpacityMode;
     ofParameter<bool>                   paramMinMaxBlend;
-    ofParameter<bool>                   paramLinearDistribution;
+    //ofParameter<int>                    paramOpacityMode;
+    //ofParameter<bool>                   paramLinearDistribution;
     ofParameter<bool>                   paramUseBPM;
     ofParameter<float>                  paramManualOffsetMs;
     ofParameter<int>                    paramOffsetBeatDiv;
     ofParameter<int>                    paramOffsetBeatMult;
-    ofParameter<ofxPm::VideoBufferNodeBased*>    paramVideoBufferInput;
-    ofParameter<vector<float>>          paramDistributionVector;
+    ofParameter<vector<float>>          paramOpacityVector;
     ofParameter<int>                    paramOversize;
-
+    ofParameter<ofxPm::VideoBufferNodeBased*>    paramVideoBufferInput;
+    //ofParameter<bool>                   paramLinearDistribution;
+    //ofParameter<vector<float>>          paramDistributionVector;
     
 private:
-
-    int             numHeaders;
-    glm::vec2       fboHasToBeAllocated;
 
     // rendering
     VideoFrame      frame;
     ofFbo           fbo;
-    bool            minmaxBlend;
-    int             opacityMode;
+    int             oldBufferSize;
+    int             desiredBufferSize;
 
 };
 }
